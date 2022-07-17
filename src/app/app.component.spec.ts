d9d859fd7a8430e6ecc0b9866e180f35c37910b8
María Fernández de la Rosa <100228799+7mariadlrosa@users.noreply.github.com>
2022-07-18 01:27:16 +0200

    Add files via upload

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,39 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { Todo } from './components/todo/todo.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent,
+        HomeComponent,
+        Todo
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'Lab-7.06-7mariadlrosa'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('Lab-7.06-7mariadlrosa');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Lab-7.06-7mariadlrosa app is running!');
+  });
+});
